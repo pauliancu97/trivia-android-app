2c13375639a19a79630b5c9c447570e9d1784c19
pauliancu97 <paul97iancu@gmail.com>
2021-10-02 13:12:54 +0300

    made settings screen UI look better

--- app/src/main/java/com/example/triviaapp/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/triviaapp/ui/screens/settings/SettingsScreen.kt
@@ -1,18 +1,16 @@
 package com.example.triviaapp.ui.screens.settings
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.RadioButton
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.triviaapp.R
 import com.example.triviaapp.ui.models.ThemeSetting
 
@@ -41,30 +39,49 @@ fun SettingsScreen(
 ) {
     Column(
         modifier = Modifier
-            .clickable { onThemeSettingClicked() }
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
     ) {
-        Text(
-            text = stringResource(R.string.Settings),
-            style = MaterialTheme.typography.h5
-        )
-        Text(
-            text = stringResource(settingsScreenState.themeSetting.textId)
-        )
+        Surface(
+            modifier = Modifier
+                .background(MaterialTheme.colors.surface)
+                .fillMaxWidth()
+                .padding(top = 5.dp)
+                .clickable { onThemeSettingClicked() },
+            elevation = 5.dp
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.Settings),
+                    style = MaterialTheme.typography.h5,
+                    color = MaterialTheme.colors.onSurface
+                )
+                Text(
+                    text = stringResource(settingsScreenState.themeSetting.textId),
+                    color = MaterialTheme.colors.onSurface
+                )
+            }
+        }
     }
     if (settingsScreenState.isThemeSettingDialogShown) {
         AlertDialog(
             onDismissRequest = { onDismissThemeSettingDialog() },
             title = {
                 Text(
-                    text = stringResource(R.string.selected_theme)
+                    text = stringResource(R.string.selected_theme),
+                    style = MaterialTheme.typography.h5
                 )
             },
             buttons = {
                 for (themeSetting in ThemeSetting.values()) {
                     Row(
-                        modifier = Modifier.clickable {
-                            onThemeSettingSelected(themeSetting)
-                        }
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(15.dp)
+                            .clickable { onThemeSettingSelected(themeSetting) }
                     ) {
                         RadioButton(
                             selected = themeSetting == settingsScreenState.themeSetting,
@@ -75,7 +92,9 @@ fun SettingsScreen(
                         )
                     }
                 }
-            }
+            },
+            modifier = Modifier
+                .background(MaterialTheme.colors.surface)
         )
     }
 }
